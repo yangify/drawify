c1e4a17d8f7556ca526af94fb1fd206caf07c69d
hong.yang <hong.yang@bytedance.com>
2022-03-16 17:34:46 +0800

    Added logic to keep prompting for user input until a quit command is received

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,29 @@
+import command.Command;
+import command.Placeholder;
+import command.Quit;
+import executor.CommandExecutor;
+import factory.CommandFactory;
+
 import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("enter command: ");
-        String rawCommand = scanner.nextLine();
+        Command command = new Placeholder();
+        CommandExecutor commandExecutor = CommandExecutor.getInstance();
+
+        while (!(command instanceof Quit)) {
+            System.out.print("enter command: ");
+            String rawCommand = scanner.nextLine();
+
+            try {
+                command = CommandFactory.create(rawCommand);
+                commandExecutor.execute(command);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
