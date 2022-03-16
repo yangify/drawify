700ee106e841ba86d7a70509744c4c2043eaa557
hong.yang <hong.yang@bytedance.com>
2022-03-16 17:33:26 +0800

    Added create method to command factory

--- src/main/java/factory/CommandFactory.java
+++ src/main/java/factory/CommandFactory.java
@@ -1,4 +1,12 @@
 package factory;
 
+import command.Command;
+import command.Create;
+import exception.InvalidCommandException;
+
 public class CommandFactory {
+
+    public static Command create(String rawCommand) throws InvalidCommandException {
+        return new Create();
+    }
 }
