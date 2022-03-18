f043378fdbbad66a80e43bbd02ac694f166047ac
yangify <hongyangprivate@gmail.com>
2022-03-18 13:31:17 +0800

    Added test for command factory

--- src/test/java/factory/CommandFactoryTest.java
+++ src/test/java/factory/CommandFactoryTest.java
@@ -1,7 +1,6 @@
 package factory;
 
-import command.Command;
-import command.Create;
+import command.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,10 +15,49 @@ public class CommandFactoryTest {
     }
 
     @Test
+    public void whenCommandUnknownReturnNull() {
+        String rawCommand = "J 13 14";
+        Command create = CommandFactory.create(rawCommand);
+        assert create == null;
+    }
+
+    @Test
     public void whenCreateCommandValidReturnsCreate() {
-        String rawCommand = "C 5 5";
+        String rawCommand = "C 8 8";
         Command create = CommandFactory.create(rawCommand);
         assert create != null;
         assertEquals(create.getClass(), Create.class);
+    }
+
+    @Test
+    public void whenDrawLineCommandValidReturnsDrawLine() {
+        String rawCommand = "L 1 1 2 2";
+        Command create = CommandFactory.create(rawCommand);
+        assert create != null;
+        assertEquals(create.getClass(), Draw.class);
+    }
+
+    @Test
+    public void whenDrawShapeCommandValidReturnsDrawShape() {
+        String rawCommand = "R 1 1 2 2";
+        Command create = CommandFactory.create(rawCommand);
+        assert create != null;
+        assertEquals(create.getClass(), Draw.class);
+    }
+
+    @Test
+    public void whenPaintCommandValidReturnsPaint() {
+        String rawCommand = "B 1 1";
+        Command create = CommandFactory.create(rawCommand);
+        assert create != null;
+        assertEquals(create.getClass(), Paint.class);
+    }
+
+    @Test
+    public void whenQuitCommandValidReturnsQuit() {
+        String rawCommand = "Q";
+        Command create = CommandFactory.create(rawCommand);
+        assert create != null;
+        assertEquals(create.getClass(), Quit.class);
     }
 }
