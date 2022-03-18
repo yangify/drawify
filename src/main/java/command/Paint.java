8f7d16af14b8170c5f2a610d66520039e9c63bad
yangify <hongyangprivate@gmail.com>
2022-03-18 15:32:08 +0800

    Added Paint command

--- src/main/java/command/Paint.java
+++ src/main/java/command/Paint.java
@@ -1,9 +1,29 @@
 package command;
 
+import canvas.Point;
+import exception.InvalidParameterException;
+
 import java.util.List;
 
 public class Paint implements Command {
-    public Paint(List<String> parameters) {
 
+    private final Point point;
+
+    public Paint(List<String> parameters) {
+        if (parameters == null || parameters.size() < 2)
+            throw new InvalidParameterException("Both x and y is required");
+
+        try {
+            int x = Integer.parseInt(parameters.get(0));
+            int y = Integer.parseInt(parameters.get(1));
+            if (x < 0 || y < 0) throw new NumberFormatException();
+            this.point = new Point(x, y);
+        } catch (NumberFormatException e) {
+            throw new InvalidParameterException("Both x and y must be positive digit");
+        }
+    }
+
+    public Point getPoint() {
+        return this.point;
     }
 }
---
+++ src/test/java/command/PaintTest.java
@@ -0,0 +1,67 @@
+package command;
+
+import canvas.Point;
+import exception.InvalidParameterException;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class PaintTest {
+
+    @Test
+    public void whenParameterIsNull_thenThrowException() {
+        List<String> parameters = null;
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Paint(parameters));
+
+        String expectedMessage = "Both x and y is required";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsInsufficient_thenThrowException() {
+        List<String> parameters = List.of("10");
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Paint(parameters));
+
+        String expectedMessage = "Both x and y is required";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsWord_thenThrowException() {
+        List<String> parameters = List.of("ten", "eight");
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Paint(parameters));
+
+        String expectedMessage = "Both x and y must be positive digit";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsNegative_thenThrowException() {
+        List<String> parameters = List.of("-10", "8");
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Paint(parameters));
+
+        String expectedMessage = "Both x and y must be positive digit";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsValid_thenReturnCreate() {
+        List<String> parameters = List.of("10", "100");
+        Paint paint = new Paint(parameters);
+
+        Point expectedPoint = new Point(10, 100);
+        Point actualPoint = paint.getPoint();
+        assertEquals(expectedPoint, actualPoint);
+    }
+}
