4b1331cfa6ea5a864f0063394c6dfa39008aa718
yangify <hongyangprivate@gmail.com>
2022-03-18 13:35:06 +0800

    Removed redundant exception

--- src/main/java/exception/InvalidCommandException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InvalidCommandException extends RuntimeException {
-    public InvalidCommandException(String message) {
-        super(message);
-    }
-}
--- src/main/java/factory/CommandFactory.java
+++ src/main/java/factory/CommandFactory.java
@@ -1,7 +1,6 @@
 package factory;
 
 import command.*;
-import exception.InvalidCommandException;
 import sketch.line.Line;
 import sketch.shape.Rectangle;
 
@@ -11,10 +10,8 @@ public class CommandFactory {
 
     static String delimiter = " ";
 
-    public static Command create(String rawCommand) throws InvalidCommandException {
+    public static Command create(String rawCommand) {
         List<String> splitCommand = List.of(rawCommand.split(delimiter));
-        if (splitCommand.size() == 0) throw new InvalidCommandException("Command cannot be empty");
-
         List<String> parameters = splitCommand.subList(1, splitCommand.size());
         switch (splitCommand.get(0)) {
             case "C": return new Create(parameters);
