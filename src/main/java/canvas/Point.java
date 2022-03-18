795c8bc3d43182602197fbb7fb430443cfd19d33
yangify <hongyangprivate@gmail.com>
2022-03-18 14:37:52 +0800

    Override equals and hashcode of Point

--- src/main/java/canvas/Point.java
+++ src/main/java/canvas/Point.java
@@ -1,5 +1,7 @@
 package canvas;
 
+import java.util.Objects;
+
 public class Point {
 
     private final int x;
@@ -16,5 +18,19 @@ public class Point {
 
     public int getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Point)) return false;
+
+        Point that = (Point) o;
+        return this.x == that.x && this.y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.x, this.y);
     }
 }
