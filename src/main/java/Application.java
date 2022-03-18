3b0b18008f6d322d26c6859e8488d5780bd7446f
yangify <hongyangprivate@gmail.com>
2022-03-18 17:02:12 +0800

    Added application logic

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,29 +1,39 @@
-import command.Command;
-import command.Placeholder;
-import command.Quit;
-import executor.CommandExecutor;
+import artist.Artist;
+import canvas.Canvas;
+import command.*;
+import factory.CanvasFactory;
 import factory.CommandFactory;
 
 import java.util.Scanner;
 
 public class Application {
 
+    private static Canvas canvas;
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         Command command = new Placeholder();
-        CommandExecutor commandExecutor = CommandExecutor.getInstance();
 
         while (!(command instanceof Quit)) {
-            System.out.print("enter command: ");
-            String rawCommand = scanner.nextLine();
-
             try {
+                prompt();
+                String rawCommand = scanner.nextLine();
                 command = CommandFactory.create(rawCommand);
-                commandExecutor.execute(command);
+                execute(command);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    public static void execute(Command command) {
+        if (command instanceof Create) canvas = CanvasFactory.execute((Create) command);
+        if (command instanceof Draw) Artist.execute(canvas, command);
+        if (command instanceof Paint) Artist.execute(canvas, command);
+    }
+
+    public static void prompt() {
+        System.out.print("enter command: ");
     }
 }
