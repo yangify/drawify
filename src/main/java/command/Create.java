d442538badd51a7c1af016da37ec3c442ae05a50
yangify <hongyangprivate@gmail.com>
2022-03-18 14:10:40 +0800

    Implemented Create

--- src/main/java/command/Create.java
+++ src/main/java/command/Create.java
@@ -6,7 +6,29 @@ import java.util.List;
 
 public class Create implements Command {
 
+    private final int rows;
+    private final int columns;
+
     public Create(List<String> parameters) {
-        if (parameters.size() < 2) throw new InvalidParameterException("Both x and y is required");
+        if (parameters == null || parameters.size() < 2)
+            throw new InvalidParameterException("Both row and column is required");
+
+        try {
+            this.rows = Integer.parseInt(parameters.get(0));
+            this.columns = Integer.parseInt(parameters.get(1));
+        } catch (NumberFormatException e) {
+            throw new InvalidParameterException("Both row and column must be positive digit");
+        }
+
+        if (this.rows < 0 || this.columns < 0)
+            throw new InvalidParameterException("Both row and column must be positive digit");
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
     }
 }
---
+++ src/test/java/command/CreateCommandTest.java
@@ -0,0 +1,70 @@
+package command;
+
+import exception.InvalidParameterException;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class CreateCommandTest {
+
+    @Test
+    public void whenParameterIsNull_thenThrowException() {
+        List<String> parameters = null;
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Create(parameters));
+
+        String expectedMessage = "Both row and column is required";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsInsufficient_thenThrowException() {
+        List<String> parameters = List.of("10");
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Create(parameters));
+
+        String expectedMessage = "Both row and column is required";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsWord_thenThrowException() {
+        List<String> parameters = List.of("ten", "eight");
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Create(parameters));
+
+        String expectedMessage = "Both row and column must be positive digit";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsNegative_thenThrowException() {
+        List<String> parameters = List.of("-10", "8");
+        Exception exception = assertThrows(InvalidParameterException.class, () -> new Create(parameters));
+
+        String expectedMessage = "Both row and column must be positive digit";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void whenParameterIsValid_thenReturnCreate() {
+        List<String> parameters = List.of("10", "100");
+        Create create = new Create(parameters);
+
+        int expectedRows = 10;
+        int actualRows = create.getRows();
+        assertEquals(expectedRows, actualRows);
+
+        int expectedColumns = 100;
+        int actualColumns = create.getColumns();
+        assertEquals(expectedColumns, actualColumns);
+    }
+}
