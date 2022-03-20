44a8033068d67ccf8c5c11feeff4d05f9fa7c65e
yangify <hongyangprivate@gmail.com>
2022-03-20 16:19:14 +0800

    Added painter

--- src/main/java/illustrator/Painter.java
+++ src/main/java/illustrator/Painter.java
@@ -3,9 +3,34 @@ package illustrator;
 import canvas.Canvas;
 import canvas.Point;
 
+import java.util.*;
+
 public class Painter extends Illustrator {
 
-    public static void paint(Canvas canvas, Point point) {
+    private static final String VALUE = "o";
 
+    public static void paint(Canvas canvas, Point point) {
+        if (isExceedCanvas(canvas, point)) throw new IllegalArgumentException("Point outside of canvas");
+
+        Set<Point> visited = new HashSet<>();
+        Stack<Point> stack = new Stack<>();
+        stack.push(point);
+
+        while (!stack.isEmpty()) {
+            Point p = stack.pop();
+            if (visited.contains(p) || !canvas.getPoint(p).equals(" ")) continue;
+
+            visited.add(p);
+            canvas.setPoint(p, VALUE);
+
+            int x = p.getX();
+            int y = p.getY();
+
+            List<Point> neighbours = List.of(new Point(x + 1, y), new Point(x - 1, y), new Point(x, y + 1), new Point(x, y - 1));
+            for (Point neighbour : neighbours) {
+                if (isExceedCanvas(canvas, neighbour) || !canvas.getPoint(neighbour).equals(" ")) continue;
+                stack.push(neighbour);
+            }
+        }
     }
 }
---
+++ src/test/java/illustrator/PainterTest.java
@@ -0,0 +1,75 @@
+package illustrator;
+
+import canvas.Canvas;
+import canvas.Point;
+import org.junit.Test;
+import sketch.line.Line;
+import sketch.shape.Rectangle;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class PainterTest {
+
+    @Test
+    public void whenNoObstacle_thenPaintAll() {
+        Canvas canvas = new Canvas(5, 5);
+        Point point = new Point(2, 2);
+        Painter.paint(canvas, point);
+
+        String expected = "-------\n" +
+                "|ooooo|\n" +
+                "|ooooo|\n" +
+                "|ooooo|\n" +
+                "|ooooo|\n" +
+                "|ooooo|\n" +
+                "-------\n";
+        String actual = canvas.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenObstacleAndOutside_thenPaintAround() {
+        Canvas canvas = new Canvas(5, 5);
+        ShapeArtist.draw(canvas, new Rectangle(List.of("1", "1", "3", "3")));
+        Point point = new Point(4, 4);
+        Painter.paint(canvas, point);
+
+        String expected = "-------\n" +
+                "|xxxoo|\n" +
+                "|x xoo|\n" +
+                "|xxxoo|\n" +
+                "|ooooo|\n" +
+                "|ooooo|\n" +
+                "-------\n";
+        String actual = canvas.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenObstacleAndInside_thenPaintWithin() {
+        Canvas canvas = new Canvas(5, 5);
+        ShapeArtist.draw(canvas, new Rectangle(List.of("1", "1", "3", "3")));
+        LineArtist.draw(canvas, new Line(List.of("4", "4", "4", "5")));
+
+        Point point1 = new Point(1, 1);
+        Painter.paint(canvas, point1);
+
+        Point point2 = new Point(0, 3);
+        Painter.paint(canvas, point2);
+
+        String expected = "-------\n" +
+                "|xxx  |\n" +
+                "|xox  |\n" +
+                "|xxx  |\n" +
+                "|ooox |\n" +
+                "|ooox |\n" +
+                "-------\n";
+        String actual = canvas.toString();
+
+        assertEquals(expected, actual);
+    }
+}
