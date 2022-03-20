ac327723b188150569a5ce055011366a38e9f406
yangify <hongyangprivate@gmail.com>
2022-03-20 14:37:06 +0800

    Removed redundant point method

--- src/test/java/canvas/PointTest.java
+++ src/test/java/canvas/PointTest.java
@@ -12,37 +12,4 @@ public class PointTest {
         Point p2 = new Point(1, 2);
         assertEquals(p1, p2);
     }
-
-    @Test
-    public void whenPointBefore_thenReturnTrue() {
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(2, 3);
-        assertTrue(p1.isBefore(p2));
-
-        Point p3 = new Point(1, 3);
-        assertTrue(p1.isBefore(p3));
-
-        Point p4 = new Point(2, 2);
-        assertTrue(p1.isBefore(p4));
-    }
-
-    @Test
-    public void whenPointNotBefore_thenReturnFalse() {
-        Point p1 = new Point(2, 3);
-        Point p2 = new Point(1, 2);
-        assertFalse(p1.isBefore(p2));
-
-        Point p3 = new Point(1, 3);
-        assertFalse(p1.isBefore(p3));
-
-        Point p4 = new Point(2, 2);
-        assertFalse(p1.isBefore(p4));
-    }
-
-    @Test
-    public void whenPointOverlaps_thenReturnFalse() {
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(1, 2);
-        assertFalse(p2.isBefore(p1));
-    }
 }
